4a309f148b5eff22b38f2ebe8b4e0405bb5399bb
broyettore <broyettore@gmail.com>
2023-03-01 16:32:20 +0100

    Palindroma done

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,2 +1,19 @@
 'use strict';
 
+function showPalindroma(parola) {
+    let palindroma = ''
+    for (let i = parola.length - 1; i >= 0; i--) {
+      palindroma += parola[i]
+    }
+    return palindroma
+  }
+  
+  const userInput = prompt("Inserisci una parola");
+
+  if (userInput === showPalindroma(userInput)) {
+    console.log(showPalindroma(userInput));
+    alert("la parola è palindroma");
+  } else {
+    alert("la parola non è palindroma");
+  }
+
