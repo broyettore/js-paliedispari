fd3dbdcaec5fdcdd28e39ef6b79e8873c92d1ff2
broyettore <broyettore@gmail.com>
2023-03-01 16:39:42 +0100

    Comments added

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/******** 
+   * Funzioni
+   *
+   *  ***** */
+
+// funzione controlla lettere per lettera dalla fine della parola
 function showPalindroma(parola) {
     let palindroma = ''
     for (let i = parola.length - 1; i >= 0; i--) {
@@ -7,9 +13,15 @@ function showPalindroma(parola) {
     }
     return palindroma
   }
-  
+
+  /******** 
+   * MAIN
+   *
+   *  ***** */
+
   const userInput = prompt("Inserisci una parola");
 
+  // condizione per vedere se la parola inserita è palindroma
   if (userInput === showPalindroma(userInput)) {
     console.log(showPalindroma(userInput));
     alert("la parola è palindroma");
--- Pari-Dispari/js/main.js
+++ Pari-Dispari/js/main.js
@@ -5,10 +5,12 @@
  *
  *********/
 
+// funzione genera numero random da 1 a 5
 function randomInterval() { 
     return Math.floor(Math.random() * (5 - 1 + 1) + 1)
   }
-  
+
+// funzione per stabilire il vincitore in base a pari o dispari
 function showWinner(player, computer) {
     let sum = player + computer;
 
@@ -27,6 +29,7 @@ const computerHand = randomInterval();
 let boolean = false;
 let playerHand = "";
 
+// ciclo infinito se la risposta non corrisponde a quelle stabbilite
 for (let i = 0; i < Infinity; i++) {
     let choice = prompt("pari o dispari");
 
@@ -39,11 +42,13 @@ for (let i = 0; i < Infinity; i++) {
     } 
 }
 
+// Se la var booleana è vera, il giocatore mette un num e la funzione parte
 if (boolean = true) {
     playerHand = Number(prompt("Inserisci un numero da 1 a 5"));
     alert(showWinner(playerHand, computerHand));
 }
 
+// se il giocatore non inserisce un num vaildo il pc vince di default
 if (isNaN(playerHand)) {
     alert("Non hai inserito un numero, il computer vince")
 };
