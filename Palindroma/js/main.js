42f71492d9a1b6b8f695963225101aea85b4f8cb
broyettore <broyettore@gmail.com>
2023-03-02 11:03:10 +0100

    palindrome function upadated

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -11,7 +11,11 @@ function showPalindroma(parola) {
     for (let i = parola.length - 1; i >= 0; i--) {
       palindroma += parola[i]
     }
-    return palindroma
+
+    if(parola === palindroma) {
+      return true
+    }
+    return false
   }
 
   /******** 
@@ -19,13 +23,4 @@ function showPalindroma(parola) {
    *
    *  ***** */
 
-  const userInput = prompt("Inserisci una parola");
-
-  // condizione per vedere se la parola inserita è palindroma
-  if (userInput === showPalindroma(userInput)) {
-    console.log(showPalindroma(userInput));
-    alert("la parola è palindroma");
-  } else {
-    alert("la parola non è palindroma");
-  }
 
